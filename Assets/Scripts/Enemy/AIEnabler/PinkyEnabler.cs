31fa2a69dfddbd7f0e44959ee71d6c533cf9cdff
Durp076 <derpstudioentertainment@gmail.com>
2022-05-25 21:30:05 +0100

    feat(AI) - Boss Enablers
    - Implemented enablers for bosses AI

---
+++ Assets/Scripts/Enemy/AIEnabler/PinkyEnabler.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinkyEnabler : MonoBehaviour
+{
+    public GameObject Pinky;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" &&
+            Pinky != null)
+        {
+            Pinky.GetComponent<BossController_Revenant>().IsActive = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player" &&
+            Pinky != null)
+        {
+            Pinky.GetComponent<BossController_Revenant>().IsActive = false;
+        }
+    }
+}
