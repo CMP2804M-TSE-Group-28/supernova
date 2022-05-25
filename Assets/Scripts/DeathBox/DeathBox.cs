222e5b9288fe4953a2ad2cb5bb518b83f395b022
Josh Cooper <hi@joshc.uk>
2022-05-25 03:46:22 +0100

    feat(death): Made the player die
    Player dies and the deathboxes make death

--- Assets/Scripts/DeathBox/DeathBox.cs
+++ Assets/Scripts/DeathBox/DeathBox.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 
 public class DeathBox : MonoBehaviour
 {
+    /// <summary>
+    /// Kill the player/enemy if they enter a death zone.
+    /// This is used on objects that are big holes in the map.
+    /// </summary>
+    /// <param name="other">The person that entered the death zone.</param>
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            // Subtract player health here
-            // I.E PlayerHealth = -69
             other.GetComponent<Health>().Kill();
+        }
+        else if (other.CompareTag("Enemy"))
+        {
+            other.GetComponent<EnemyController>().Health = 0;
         }
     }
 }
