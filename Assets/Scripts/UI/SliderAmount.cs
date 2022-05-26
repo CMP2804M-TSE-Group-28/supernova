2b3006815f123c907973a935d594b4708b21929c
RROUOL <79855173+RROUOL@users.noreply.github.com>
2022-05-26 07:50:09 +0100

    feat: area names added to UI

---
+++ Assets/Scripts/UI/SliderAmount.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderAmount : MonoBehaviour
+{
+    public static SliderAmount main;
+
+    public Text text;
+
+    public Slider sliderObject;
+
+    public int maxAmount;
+
+    public int _amount;
+
+    private void Awake()
+    {
+        main = this;
+    }
+
+    public void ChangeValue()
+    {
+        _amount = (int) (sliderObject.value);
+        if (_amount >= maxAmount)
+        {
+            text.text = "MAX";
+        }
+        else
+        {
+            //main.GetComponent<UnityEngine.UI.Text>().text = "" + _amount;
+            text.text = "" + _amount;
+        }
+    }
+}
